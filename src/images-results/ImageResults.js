2c4a37108955864216f4db56fa21bcb9c8d79c02
Rudi Starcevic <rudi.starcevic@viseo.com>
2018-04-25 14:26:22 +0800

    Initial commit

---
+++ src/images-results/ImageResults.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { GridList, GridTile } from "material-ui/GridList";
+import IconButton from "material-ui/IconButton";
+import ZoomIn from "material-ui/svg-icons/action/zoom-in";
+import Dialog from "material-ui/Dialog";
+import FlatButton from "material-ui/FlatButton";
+
+class ImageResults extends Component {
+  render() {
+    let imageListContent;
+    const { images } = this.props;
+    if (images) {
+      imageListContent = (
+        <GridList cols={3}>
+          {images.map(img => (
+            <GridTile
+              key={img.id}
+              title={img.tags}
+              subtitle={
+                <span>
+                  by <b>{img.user}</b>
+                </span>
+              }
+              actionIcon={
+                <IconButton>
+                  <ZoomIn color="white" />
+                </IconButton>
+              }
+            >
+            <img src={img.largeImageURL} alt="Pixabay" />
+            </GridTile>
+          ))}
+        </GridList>
+      );
+    } else {
+      imageListContent = null;
+    }
+    return <div>{imageListContent}</div>;
+  }
+}
+
+ImageResults.propTypes = {
+  images: PropTypes.array.isRequired
+};
+
+export default ImageResults;
---
+++ src/navbar/NavBar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import AppBar from "material-ui/AppBar";
+
+const NavBar = () => (
+  <AppBar title="PixaBay Image Finder v1.0" showMenuIconButton={false} />
+);
+
+export default NavBar;
