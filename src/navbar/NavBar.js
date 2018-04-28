70c1614f5dd64535e8b3f2f0884defa850666043
Rudi Starcevic <rudi.starcevic@viseo.com>
2018-04-28 19:57:36 +0800

    Update to single index.html

--- src/navbar/NavBar.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import AppBar from "material-ui/AppBar";
-
-const NavBar = () => (
-  <AppBar title="PixaBay Image Finder v1.0" showMenuIconButton={false} />
-);
-
-export default NavBar;
